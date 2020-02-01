7baa9115b73abdcbd0e4b422d9b962ed8deb4e0a
Javier Gamarra <nhpatt@gmail.com>
2020-02-01 20:32:06 +0100

    Naive functional component

--- src/exercises/functionalComponent/Exercise.js
+++ src/exercises/functionalComponent/Exercise.js
@@ -1,4 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+const Component = () => {
+
+  const [messages, setMessages] = useState(["mounted"]);
+
+  useEffect(() => {
+    messages.push("updated");
+  });
+
+  useEffect(() => {
+    return () => {
+      setMessages("unmounting");
+    }
+  }, []);
+
+  messages.push("rendered");
+
+  return (
+    <div>
+      I use "lifecycle hooks" under the covers.
+      <ul>
+        {messages.map((message, i) => {
+          return <li key={i}>{message}</li>;
+        })}
+      </ul>
+      <button onClick={() => setMessages([...messages])}>Refresh</button>
+    </div>
+  );
+};
 
 export default function Exercise() {
   return (
@@ -11,46 +40,8 @@ export default function Exercise() {
         </p>
       </div>
       <div className="exercise">
-        <Component />
+        <Component/>
       </div>
     </>
   );
-}
-
-class Component extends React.Component {
-  constructor(props) {
-    super(props);
-    this.messages = [];
-  }
-
-  componentDidMount() {
-    this.messages.push("mounted");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    this.messages.push("updated");
-  }
-
-  componentWillUnmount() {
-    // We won't actually get to see this one, but including it for completeness.
-    this.messages.push("unmounting");
-  }
-
-  render() {
-    this.messages.push("rendered");
-
-    const messages = this.messages;
-
-    return (
-      <div>
-        I use "lifecycle hooks" under the covers.
-        <ul>
-          {messages.map((message, i) => {
-            return <li key={i}>{message}</li>;
-          })}
-        </ul>
-        <button onClick={() => this.forceUpdate()}>Refresh</button>
-      </div>
-    );
-  }
 }
